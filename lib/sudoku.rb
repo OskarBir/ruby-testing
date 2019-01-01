4cf342723875d01554097294490d54ea026667dd
OskarBir <oskar.bir@gmailc.om>
2019-01-01 18:53:58 +0100

    more specs

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,5 +1,6 @@
-class Sudoku 
+class Sudoku
   attr_reader :board
+
   def initialize(string_input)
     validate(string_input)
     @string_input = string_input
@@ -16,11 +17,11 @@ class Sudoku
   end
 
 
-   def get_row(row_index)
-    @array_of_rows[row_index] 
+  def get_row(row_index)
+    @array_of_rows[row_index]
   end
- 
- # The transpose method creates an array with elements with same index #
+
+  # The transpose method creates an array with elements with same index #
   def get_column(col_index)
     @array_of_rows.transpose[col_index]
   end
@@ -29,51 +30,51 @@ class Sudoku
   def board
     (0..8).to_a.each do |i|
       puts
-      puts "- " * 12 if i%3==0
-      (0..8).to_a.each do |j|  
-        print "| " if j%3==0
+      puts "- " * 12 if i % 3 == 0
+      (0..8).to_a.each do |j|
+        print "| " if j % 3 == 0
         print @array_of_rows[i][j].to_s + " "
       end
     end
   end
 
-  def nine_box_grid(row_index,col_index)
-    row_off_set =   row_index - (row_index % 3)
+  def nine_box_grid(row_index, col_index)
+    row_off_set = row_index - (row_index % 3)
     col_off_set = col_index - (col_index % 3)
     box_array = []
 
-    x= 0
+    x = 0
     while x < 3
-      y=0
+      y = 0
       while y < 3
         box_array << @array_of_rows[row_off_set + x][col_off_set + y]
-        y+=1
+        y += 1
       end
-      x+=1
+      x += 1
     end
-    return box_array
+    box_array
   end
-   
+
   def sudoku_solve!
     position_checked = 0
     while @array_of_rows.flatten.include?(0)
-      @array_of_rows.each_with_index do |row, row_index| 
+      @array_of_rows.each_with_index do |row, row_index|
         row.each_with_index do |column, col_index|
           if position_checked > 90
             raise ArgumentError, "no solution"
           end
           if @array_of_rows[row_index][col_index] == 0
-             @solutions = (1..9).to_a
-             @solutions -= nine_box_grid(row_index,col_index)  
-             @solutions -= get_row(row_index)  
-             @solutions -= get_column(col_index)
-             if @solutions.size == 1
+            @solutions = (1..9).to_a
+            @solutions -= nine_box_grid(row_index, col_index)
+            @solutions -= get_row(row_index)
+            @solutions -= get_column(col_index)
+            if @solutions.size == 1
               @array_of_rows[row_index][col_index] = @solutions.join("").to_i
               position_checked = 0
               puts
               board
               puts
-             end
+            end
           end
           position_checked += 1
         end
@@ -83,10 +84,11 @@ class Sudoku
 
 
   private
+
   def validate(string_input)
-    if string_input.length > 81 
+    if string_input.length > 81
       raise ArgumentError, "Your input must be 81 character long and currently it is greater than 81"
-    elsif string_input.length < 81 
+    elsif string_input.length < 81
       raise ArgumentError, " Your input must be 81 character long and currently it is less than 81"
     end
   end
--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -2,7 +2,10 @@ require_relative '../lib/sudoku'
 
 RSpec.describe Sudoku do
 
-  let(:sudoku) { Sudoku.new("530070000600195000098000060800060003400803001700020006060000280000419005000080079") }
+  let(:sudoku) {Sudoku.new("530070000600195000098000060800060003400803001700020006060000280000419005000080079")}
+  let(:too_hard_sudoku) {Sudoku.new("000600070000043090600000003850000000720050069364900081000830004000090035037000600")}
+  let(:too_short_sudoku) {Sudoku.new("123456")}
+  let(:too_long_sudoku) {Sudoku.new("0006000700000430906000000038500000007200500693649000810008300040000900350370006003245")}
   context "#create rows" do
     it "checks if array is created" do
       expect(sudoku.create_rows).to be_a_kind_of(Array)
@@ -23,7 +26,7 @@ RSpec.describe Sudoku do
     end
 
     it "checks if row is a valid column" do
-      expect(sudoku.get_row(0)).to match_array([5,3,0,0,7,0,0,0,0])
+      expect(sudoku.get_row(0)).to match_array([5, 3, 0, 0, 7, 0, 0, 0, 0])
     end
   end
 
@@ -33,18 +36,18 @@ RSpec.describe Sudoku do
     end
 
     it "checks if column is a valid column" do
-      expect(sudoku.get_column(0)).to match_array([5,6,0,8,4,7,0,0,0])
+      expect(sudoku.get_column(0)).to match_array([5, 6, 0, 8, 4, 7, 0, 0, 0])
     end
   end
 
   context "#board" do
 
     it "checks if there is any output" do
-      expect { sudoku.board }.to output.to_stdout
+      expect {sudoku.board}.to output.to_stdout
     end
 
     it "checks if the output matches the expected output" do
-      expect { sudoku.board }.to output("\n- - - - - - - - - - - - \n| 5 3 0 | 0 7 0 | 0 0 0 \n| 6 0 0 | 1 9 5 | 0 0 0 \n| 0 9 8 | 0 0 0 | 0 6 0 \n- - - - - - - - - - - - \n| 8 0 0 | 0 6 0 | 0 0 3 \n| 4 0 0 | 8 0 3 | 0 0 1 \n| 7 0 0 | 0 2 0 | 0 0 6 \n- - - - - - - - - - - - \n| 0 6 0 | 0 0 0 | 2 8 0 \n| 0 0 0 | 4 1 9 | 0 0 5 \n| 0 0 0 | 0 8 0 | 0 7 9 ").to_stdout
+      expect {sudoku.board}.to output("\n- - - - - - - - - - - - \n| 5 3 0 | 0 7 0 | 0 0 0 \n| 6 0 0 | 1 9 5 | 0 0 0 \n| 0 9 8 | 0 0 0 | 0 6 0 \n- - - - - - - - - - - - \n| 8 0 0 | 0 6 0 | 0 0 3 \n| 4 0 0 | 8 0 3 | 0 0 1 \n| 7 0 0 | 0 2 0 | 0 0 6 \n- - - - - - - - - - - - \n| 0 6 0 | 0 0 0 | 2 8 0 \n| 0 0 0 | 4 1 9 | 0 0 5 \n| 0 0 0 | 0 8 0 | 0 7 9 ").to_stdout
     end
   end
 
@@ -52,17 +55,44 @@ RSpec.describe Sudoku do
   context "#nine_box_grid" do
 
     it "checks if array is returned" do
-      expect(sudoku.nine_box_grid(0,0)).to be_a_kind_of(Array)
+      expect(sudoku.nine_box_grid(0, 0)).to be_a_kind_of(Array)
     end
 
     it "checks if the correct array is returned" do
-      expect(sudoku.nine_box_grid(4,4)).to match_array([0,6,0,8,0,3,0,2,0])
+      expect(sudoku.nine_box_grid(4, 4)).to match_array([0, 6, 0, 8, 0, 3, 0, 2, 0])
+    end
+
+    it "checks if the size of array is valid" do
+      expect(sudoku.nine_box_grid(8, 8).size).to eq(9)
     end
 
   end
 
+  context "#sudoku_solve!" do
 
+    it "checks if there is any output" do
+      expect {sudoku.sudoku_solve!}.to output.to_stdout
+    end
 
+    it "checks if function returns nil" do
+      expect(sudoku.sudoku_solve!).to be_nil
+    end
 
+    it "checks if error is raised when there is no solution" do
+      expect {too_hard_sudoku.sudoku_solve!}.to raise_error(ArgumentError, "no solution")
+    end
 
+  end
+
+  context "#validate" do
+
+    it "checks if error is raise when sudoku is too short" do
+      expect {too_short_sudoku}.to raise_error(ArgumentError, " Your input must be 81 character long and currently it is less than 81")
+    end
+
+    it "checks if error is raise when sudoku is too long" do
+      expect {too_long_sudoku}.to raise_error(ArgumentError, "Your input must be 81 character long and currently it is greater than 81")
+    end
+
+  end
 end
